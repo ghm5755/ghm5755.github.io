e6c7c2880cf959726f8fec38b5faadbea2879aa9
ghm5755 <ghm5755@g.rit.edu>
2023-05-14 22:47:37 -0700

    php only

--- about.php
+++ about.php
@@ -3,17 +3,42 @@
     include "assets/include/header.php"
     
 ?>
+        
         <section class="me">
             <img src="./assets/images/gavin.jpg" alt="picture">
         <h2>About Me</h2>
             <p>
-                My name is Gavin Miyagawa and am currently a second year at RIT 
+                This page is very much a work in progress and needs a lot of cleaning up
+            </p>
+            <p>
+                My name is Gavin Miyagawa and am currently going into my third year at RIT 
                 in the Web and Mobile Computing program. I am from Encinitas, California and 
                 enjoy backpacking, running, reading, and video games.
+                <br>
+                <br>
                 <a href="troop2kgavin@gmail.com">Email: troop2kgavin@gmail.com</a>
-		<a href="https://www.linkedin.com/in/gavin-miyagawa-75ba31231/">Linkedin</a>
-		<a>Phone:(760) 500 - 8573</a>
-            </p>   
-        </section>       
+            </p>
+             
+        </section>    
+        <section class="work">
+            <h2>Projects and Classes</h2>
+            <br><br><br><br>
+                <h4>Year One</h4>
+                <ul>
+                    <li><a href="">ISTE 140 - On school network</a></li>
+                    <li><a href="https://people.rit.edu/ghm5755/ISTE240/">ISTE 240</a></li>
+                    <li><a href="https://github.com/alex391/group-2-unix-survival">Unix Tutorial - Group Project</a></li>
+                </ul>
+                <h4>Year Two</h4>
+                <ul>
+                    <li><a href="https://github.com/sb22322002/NUTRiAPP/releases/tag/verison1.0">Group Java Project</a></li>
+                </ul>
+                <h4> Individual Projects</h4>
+                <ul>
+                    <li><a href="">To be linked</a></li>
+                </ul>
+            
+        </section>
+         
     </body>
 </html>
