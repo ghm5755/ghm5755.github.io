c34d9bbf7f1127127b8aa8d0cf627ddb867c4cc7
ghm5755 <ghm5755@g.rit.edu>
2023-06-08 19:33:07 -0700

    up

---
+++ assets/include/header.php
@@ -0,0 +1,33 @@
+
+<!--Gavin Miyagawa -->
+<!DOCTYPE HTML>
+<html lang=en>
+    <head>
+        <meta charset = "utf-8">   
+        <title><?php echo $pageTitle; ?></title>
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <script src="assets/script/script.js"></script> 
+    </head>
+    <body>
+        <!--<nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="yearOne.php">Year One</a>
+                    <ul>
+                      <li><a href="iste140.php">Web And Mobile I</a></li>
+                      <li><a href="csci140.php">Web And Mobile II</a></li>
+                    </ul>
+                </li>
+                <li><a href="yearTwo.php">Year Two</a>
+                    <ul>
+                        <li><a href="iste222.php">Computational Problem Solving</a></li>
+                        <li><a href="iste252.php">Foundations of Mobile Design</a></li>
+                        <li><a href="iste340.php">Client Programming</a></li>
+                    </ul>
+                </li>
+                    <li><a href="about.php">About</a>
+                </li>
+                <img src="assets/images/logo.png" alt="Logo">
+        </nav>--> 
+            
+     
